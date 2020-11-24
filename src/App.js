b3ea6391146cfed1d4121581c8b5096f2eb8eee4
bsperezg <bryan.perez@grupobootes.com>
2020-11-24 11:05:30 -0500

    MASTER: Settings and project configuration

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
+export const App = () => {
+  return (
+    <Fragment>
+      <GlobalStyles />
+      <p>App</p>
+    </Fragment>
+  );
+};
