e650ad2469602bd317016e68e884eae1472b10ae
bperezgo <bsperezgo@gmail.com>
2022-09-12 15:09:08 -0500

    refactor: switch npm for yarn

--- src/components/ListOfExperiences/index.js
+++ src/components/ListOfExperiences/index.js
@@ -1,11 +1,11 @@
-import React, { Fragment } from "react";
-import { Experience } from "../Experience";
+import React, { Fragment } from 'react';
+import { Experience } from '../Experience';
 
 export const ListOfExperiences = () => {
   return (
     <Fragment>
       {[1, 2, 3].map((exp) => (
-        <Experience />
+        <Experience key={exp} />
       ))}
     </Fragment>
   );
